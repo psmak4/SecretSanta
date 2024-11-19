b24693647f8640e15a9d7bb053b619efa11f9e71
Preston Jones <psmak4@gmail.com>
2024-11-18 22:50:52 -0700

    Update header to use auth and Gravatar for user image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,92 @@
-import { Navbar } from 'flowbite-react'
+import { User, signOut } from 'firebase/auth'
+import { Avatar, Dropdown, Navbar } from 'flowbite-react'
+import { useEffect, useState } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import AppRoutes from '../constants/AppRoutes'
+import useAuthStore from '../hooks/useAuthStore'
+import { auth } from '../lib/Firebase'
 
-const Header = () => {
-	const { pathname } = useLocation()
+type NavProps = {
+	pathname: string
+	user?: User
+}
+
+const AuthenticatedNav = ({ pathname, user }: NavProps) => {
+	const [gravatarUrl, setGravatarUrl] = useState<string>('')
+
+	const handleLogoutClick = () => {
+		signOut(auth)
+			.then(() => {
+				console.log('User logged out')
+			})
+			.catch((error) => {
+				console.log('An error occurred while logging out the current user:', error)
+			})
+	}
+
+	useEffect(() => {
+		let isAlive = true
+
+		const sha256 = async (message: string) => {
+			const encoder = new TextEncoder()
+			const data = encoder.encode(message)
+			const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+
+			const hashArray = Array.from(new Uint8Array(hashBuffer))
+			const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+
+			return hashHex
+		}
+
+		const getGravatarUrl = async () => {
+			const hashedEmail = await sha256(user?.email!)
+			const gravatarUrl = `https://www.gravatar.com/avatar/${hashedEmail}?s=40`
+
+			return gravatarUrl
+		}
+
+		getGravatarUrl().then((url: string) => {
+			if (isAlive) setGravatarUrl(url)
+		})
+
+		return () => {
+			isAlive = false
+		}
+	}, [])
 
 	return (
 		<Navbar className='shadow-md sticky top-0'>
 			<Navbar.Brand as={Link} to={AppRoutes.Home()}>
 				<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>Secret Santa</span>
 			</Navbar.Brand>
-			<Navbar.Toggle className='ml-2' />
+			<div className='flex md:order-2 gap-2'>
+				{gravatarUrl && (
+					<Dropdown arrowIcon={false} inline label={<Avatar alt='User settings' img={gravatarUrl} rounded />}>
+						<Dropdown.Header>
+							{user?.displayName && <span className='block text-sm'>{user.displayName}</span>}
+							<span className='block truncate text-sm font-medium'>{user?.email}</span>
+						</Dropdown.Header>
+						<Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
+					</Dropdown>
+				)}
+				<Navbar.Toggle />
+			</div>
+			<Navbar.Collapse>
+				<Navbar.Link as={NavLink} className='text-base font-light' to={AppRoutes.Home()} active={pathname === AppRoutes.Home()}>
+					Home
+				</Navbar.Link>
+			</Navbar.Collapse>
+		</Navbar>
+	)
+}
+
+const UnauthenticatedNav = ({ pathname }: NavProps) => {
+	return (
+		<Navbar className='shadow-md sticky top-0'>
+			<Navbar.Brand as={Link} to={AppRoutes.Home()}>
+				<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>Secret Santa</span>
+			</Navbar.Brand>
+			<Navbar.Toggle />
 			<Navbar.Collapse>
 				<Navbar.Link as={NavLink} className='text-base font-light' to={AppRoutes.Home()} active={pathname === AppRoutes.Home()}>
 					Home
@@ -24,6 +100,19 @@ const Header = () => {
 			</Navbar.Collapse>
 		</Navbar>
 	)
+}
+
+const Header = () => {
+	const { user } = useAuthStore()
+	const { pathname } = useLocation()
+
+	useEffect(() => {
+		console.log('user', user)
+	}, [user])
+
+	if (user && user.uid) return <AuthenticatedNav pathname={pathname} user={user} />
+
+	return <UnauthenticatedNav pathname={pathname} />
 }
 
 export default Header
